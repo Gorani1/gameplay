8bfcdf90dfc621dece7757d35e0421dec24f49cb
Gorani1 <us.gorani@gmail.com>
2019-10-03 21:05:36 -0500

    Updates

---
+++ precache-manifest.7b58f4c1a91faecdb82530d371a6d2ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf36e81a5521e04f054250d7e78d4307",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08cebc694b2b50799652",
+    "url": "/static/css/main.35e3cc5b.chunk.css"
+  },
+  {
+    "revision": "211127d53807bf9577be",
+    "url": "/static/js/2.a9617db8.chunk.js"
+  },
+  {
+    "revision": "08cebc694b2b50799652",
+    "url": "/static/js/main.40a7e639.chunk.js"
+  },
+  {
+    "revision": "117a1256623cab533775",
+    "url": "/static/js/runtime~main.3a1c28d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
